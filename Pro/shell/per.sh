e8d252de5f2a2e0b5eda91489c4b893a99fbf71f
sqm2050 <sqm2050@gmail.com>
2013-02-24 09:48:50 +0800

    modified:   Pro/shell/per.sh 	modified:   Study/Cavium OCTEON Hardware Reference Manual.wiki 	modified:   Study/index.wiki 	deleted:    Study/schedule.wiki 	modified:   Study/ubuntu.wiki 	modified:   Work/GTP.wiki 	modified:   Work/index.wiki 	Books/ 	Study/Display Manager.wiki 	Study/table.wiki 	Work/switch.wiki

--- Pro/shell/per.sh
+++ Pro/shell/per.sh
@@ -4,23 +4,30 @@ POS=40
 #for ((i=0; i<=100; i++)) 
 #sh does not support
 rm ./nohup.out 2>/dev/null
-nohup tcpreplay -i eth1 -l 0 -t  $1 &
 
 echo 
-echo -n "Senting Packet for 120s  ... "
 i=1
-while [ $i -lt 120 ]
-do 
-	echo -ne "\\033[${POS}G $i % completed"
-	sleep 1.2
-	i=`expr $i + 1`
-done
+if [ $2 -eq 0 ]; then
+	time=101
+	nohup tcpreplay -i eth1 -l $2 -t  $1 &
+	echo 
+	echo -n "Senting Packet for 120s  ... "
+	while [ $i -lt $time ]
+	do 
+		echo -ne "\\033[${POS}G $i % completed"
+		sleep 1.2
+		i=`expr $i + 1`
+	done
+
+	echo -ne "\n"
+	pid=`ps -ef | grep $1 | grep tcpreplay|awk '{print $2}'`
+	kill -9 $pid >/dev/null 2>&1
+	sleep 2
+
+	lines=`cat ./nohup.out |grep $1| wc -l`
+	echo "共发送 $lines 个报文"
+else
+	tcpreplay -i eth1 -l $2 -t  $1 &
+	echo -e "发送 $2 个报文\n"
+fi
 echo -ne "\n"
-
-pid=`ps -ef | grep $1 | grep tcpreplay|awk '{print $2}'`
-kill -9 $pid
-sleep 2
-echo Kill ok
-
-lines=`cat ./nohup.out |grep $1| wc -l`
-echo "共发送 $lines 个报文"
